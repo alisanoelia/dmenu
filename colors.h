8553ed28aa614713c3423a35a5245bca27c34071
alyssa <alyssa>
2023-12-16 23:16:16 +0000

    update: new color scheme

--- colors.h
+++ colors.h
@@ -1,6 +1,6 @@
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#afbcb6", "#0C020C" },
-	[SchemeSel] = { "#afbcb6", "#35534b" },
-	[SchemeOut] = { "#afbcb6", "#88756C" },
+	[SchemeNorm] = { "#ffffff", "#1a1b26" },
+	[SchemeSel] = { "#ffffff", "#e0af68" },
+	[SchemeOut] = { "#ffffff", "#88756C" },
 };
