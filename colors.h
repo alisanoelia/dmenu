0a4ec3b5efaa1baaeab2e670c0c9b6360bda29cf
alyssa <alyssa>
2024-06-05 22:50:16 -0500

    update: new colorscheme

--- colors.h
+++
@@ -1,6 +0,0 @@
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#ffffff", "#1a1b26" },
-	[SchemeSel] = { "#ffffff", "#e0af68" },
-	[SchemeOut] = { "#ffffff", "#88756C" },
-};
---
+++ kolore.h
@@ -0,0 +1,6 @@
+static const char *colors[SchemeLast][2] = {
+    /*     fg         bg       */
+    [SchemeNorm] = {"#ffffff", "#0d1617"},
+    [SchemeSel] = {"#ffffff", "#35a77c"},
+    [SchemeOut] = {"#ffffff", "#1f6f47"},
+};
