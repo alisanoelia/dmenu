8ed945d09375ef1e13fd0687e0f9300174f64dc9
alyssa <alyssa>
2024-08-04 03:00:02 -0500

    update: clean repo

--- kolore.h
+++ kolore.h
@@ -1,6 +1,6 @@
 static const char *colors[SchemeLast][2] = {
     /*     fg         bg       */
-    [SchemeNorm] = {"#ffffff", "#0d1617"},
-    [SchemeSel] = {"#ffffff", "#35a77c"},
-    [SchemeOut] = {"#ffffff", "#1f6f47"},
+    [SchemeNorm] = {"#ffffff", "#051215"},
+    [SchemeSel] = {"#ffffff", "#cc8f71"},
+    [SchemeOut] = {"#ffffff", "#cc8f71"},
 };
