74fcf6c20651ae4a9e8d0d02623a459e151c5818
alyssa <alyssa>
2024-08-14 02:20:09 +0000

    update: clean

--- kolore.h
+++
@@ -1,6 +0,0 @@
-static const char *colors[SchemeLast][2] = {
-    /*     fg         bg       */
-    [SchemeNorm] = {"#ffffff", "#051215"},
-    [SchemeSel] = {"#ffffff", "#cc8f71"},
-    [SchemeOut] = {"#ffffff", "#cc8f71"},
-};
